af5b095d168cfbaa96a9bebc8ee4ce3d4afcd623
yoonkyung <kyung3098@naver.com>
2023-11-21 01:21:00 +0900

    Fix: board 기능 수정
    1. board/router.js 로 바꾸기
    board-schema || board.shema.js
    2. api url -> /categories/:name
    3. /:boardId -> /:id
    4. router.put -> 수정할 대상이 없다면 수정, 있다면 전체적으로 수정
    5. (!isLiked.checked) 로 바꾸기
    6. 로그인 검사는 미들웨어에 추가
    7. 매개변수가 많을 수록 버그 유발
    createBoard  = async (객체로 묶음 {})
    8. errMsg -> message
    9. 속성명return {} JSON으로 넘길 때는 _ 붙이기

--- src/board/router.js
+++ src/board/router.js
@@ -1,4 +1,167 @@
 const express = require('express');
 const router = express.Router();
+const { S3Client } = require('@aws-sdk/client-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const path = require('path');
+
+const boardService = require('./service');
+
+const s3 = new S3Client({
+    region : 'ap-northeast-2',
+    credentials : {
+        accessKeyId : process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey : process.env.S3_SECRET_ACCESS_KEY,
+    }
+});
+  
+const upload = multer({
+    storage: multerS3({
+    s3: s3,
+    bucket: 'mongmongvillagebucket',
+        key: function (req, file, cb) {
+        cb(null, `board/${Date.now()}_${path.basename(file.originalname)}`) //업로드시 파일명 변경가능
+    }
+    }),
+    limits: { fileSize: 20 * 1024 * 1024 }, // 20MB
+});
+
+// 로그인 검사
+router.post('/', upload.array('images'), async (req, res, next) => {
+    try {
+       const { title, content, animalType, category } = req.body;
+       const userId = '655ac300ee052627917debf7';
+       let imageUrl = [];
+       if (req.files.length > 0) {
+         imageUrl = req.files.map((file) => file.location);
+         console.log(imageUrl);
+       }
+       const createBoardResult = await boardService.createBoard({ userId, title, content, animalType, category, imageUrl });
+       return res.status(201).json(createBoardResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 로그인 검사
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const userId = '655ac300ee052627917debf7';
+        const boardId = req.params.id;
+        const deleteBoardResult = await boardService.deleteBoard(userId, boardId);
+        if (deleteBoardResult.status === 200)
+            return res.status(200).json(deleteBoardResult);
+        else
+            return res.status(400).json(deleteBoardResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+
+});
+
+// 게시글 조회 + 페이지네이션
+router.get('/', async (req, res, next) => {
+    try {
+        const currentPage = req.query.currentPage || 1;
+        const perPage = 4;
+        const allBoardsResult = await boardService.getAllBoards(currentPage, perPage);
+        return res.status(200).json(allBoardsResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/category/:name', async (req, res, next) => {
+    try {
+        const currentPage = req.query.currentPage || 1;
+        const perPage = 4;
+        const category = req.params.name;
+        const categoryBoardsResult = await boardService.getCategoryBoards(category, currentPage, perPage);
+        return res.status(200).json(categoryBoardsResult)
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/best', async (req, res, next) => {
+    try {
+        const bestBoardResult = await boardService.getBestBoards();
+        return res.status(200).json(bestBoardResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 좋아요를 눌렀다면 좋아요 취소, 아니면 좋아요 생성
+// 로그인 검사
+router.put('/:id/liked', async (req, res, next) => {
+    try {
+        const userId = '655ac300ee052627917debf7';
+        const boardId = req.params.id;
+        const isLiked = await boardService.getLiked(boardId, userId);
+        let likedBoardResult;
+
+        if (!isLiked.checked) 
+            likedBoardResult = await boardService.setLiked(boardId, userId);
+        else
+            likedBoardResult = await boardService.deleteLiked(boardId, userId);
+
+        if (likedBoardResult.status === 400) {
+            return res.status(400).json(likedBoardResult);
+        }
+        return res.status(200).json(likedBoardResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const boardId = req.params.id;
+        const boardDetailResult = await boardService.getDetailBoard(boardId);
+
+        if (boardDetailResult.status === 400) {
+            return res.status(400).json(boardDetailResult);
+        }
+        return res.status(200).json(boardDetailResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 로그인 검사
+router.get('/mypage/user', async (req, res, next) => {
+    try {
+        // 로그인 했는지 검사
+        const userId = '655ac300ee052627917debf7';
+        const userBoardsResult = await boardService.getUserBoards(userId);
+        return res.status(200).json(userBoardsResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+// 회원이 좋아요 누른 게시글 목록
+router.get('/mypage/user/liked', async (req, res, next) => {
+    try {
+        const userId = '655b7088517643964f77aa48';
+        const userLikedBoardResult = await boardService.getUserLikedBoards(userId);
+        if (res.message) {
+            res.status(404).json(userLikedBoardResult);
+        }
+        return res.status(200).json(userLikedBoardResult);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
---
+++ src/board/service.js
@@ -0,0 +1,232 @@
+const Board = require('./model/board.schema');
+const User = require('../user/model/user.schema');
+const Like = require('../board/model/like.schema');
+const Comment = require('../comment/model/comment.schema');
+
+exports.createBoard = async ({ userId, title, content, animalType, category, imageUrl }) => {
+    try {
+        animalType = animalType || null;
+        category = category || null;
+
+        const board = await Board.create({
+            user_id: userId,
+            comment_id: [],
+            title: title,
+            content: content,
+            images: imageUrl,
+            animal_type: animalType,
+            like_count: 0,
+            category: category,
+        });
+        console.log(board);
+        return {
+            status: 201,
+            board_id: board['_id'],
+            message: '게시글이 성공적으로 작성되었습니다.'
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.updateBoard = async () => {
+
+}
+
+exports.deleteBoard = async (userId, boardId) => {
+    try {
+        const result = await Board.findOneAndDelete({ _id: boardId, user_id: userId });
+        if (!result) {
+            return {
+                status: 404,
+                message: '존재하지 않는 게시글입니다.'
+            }
+        }
+        return {
+            status: 200,
+            message: '게시글이 삭제되었습니다.'
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.getAllBoards = async (currentPage, perPage) => {
+  try {
+    const total_number_of_boards = await Board.countDocuments({});
+    const boards = await Board
+                .find({})
+                .sort({createdAt: -1})
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage)
+                .populate({ path: 'user_id', select: '_id image nickname' });
+    if (boards.length === 0) {
+        return {
+            status: 404,
+            message: '게시글이 존재하지 않습니다.'
+        }
+    }
+    return {
+        status: 200,
+        total_number_of_boards,
+        boards,
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+exports.getCategoryBoards = async (category, currentPage, perPage) => {
+    try {
+        const total_number_of_boards = await Board.countDocuments({ category: category });
+        const boards = await Board 
+                    .find({ category: category })
+                    .sort({ createdAt: -1 })
+                    .skip((currentPage - 1) * perPage)
+                    .limit(perPage);
+        return {
+            status: 200,
+            total_number_of_boards, 
+            boards,
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+// 홈 페이지 상위 4개 게시글(좋아요순 기준 같으면 최신순)
+exports.getBestBoards = async () => {
+    try {
+        const boards = await Board.find({})
+                    .populate({ path: 'user_id', select: '_id nickname' })
+                    .sort('-like_count -createdAt')
+                    .limit(4)
+                    .select('_id images title like_count')
+        return {
+            status: 200,
+            boards,
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.getUserBoards = async (userId) => {
+    try {
+        const board = await Board.find({ user_id: userId });
+        if (board.length === 0) {
+            return {
+                status: 404,
+                message: "게시글이 존재하지 않습니다."
+            }
+        }
+        return {
+            status: 200,
+            board,
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.getUserLikedBoards = async (userId) => {
+    try {
+        const liked = await Like.find({ user_id: userId })
+                        .populate({ path: 'user_id', select: '_id nickname image'})
+                        .populate({ path: 'board_id', select: '_id title content createdAt updatedAt comment_id' })
+                        .select('_id user_id board_id');
+
+        if (liked.length === 0) {
+            return {
+                status: 404,
+                message: "게시글이 존재하지 않습니다."
+            }
+        }
+        return {
+            status: 200,
+            liked,
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.getDetailBoard = async (boardId) => { 
+    try {
+        const board = await Board.findOne({ _id: boardId })
+            .populate({ path: 'user_id', select: '_id nickname image' });
+        const comments = await Comment.find({ board_id: boardId })
+            .select('_id user_id content createdAt updatedAt')
+            .populate({ path: 'user_id', select: '_id image nickname' });
+
+        console.log('게시글: ', board);
+        console.log('댓글: ', comments);
+        if (!board) {
+            return {
+                status: 404,
+                message: '존재하지 않는 게시글입니다.'
+            }
+        }
+        return {
+            status: 200,
+            board,
+            comments,
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.getLiked = async (boardId, userId) => {
+    try {
+        const isLikedResult = await Like.findOne({ board_id: boardId, user_id: userId });
+        console.log(isLikedResult);
+        if (isLikedResult === null) return { checked: false };
+        else return { checked: true };
+    } catch (error) {
+        throw error;
+    }
+}
+// 좋아요 생성
+exports.setLiked = async (boardId, userId) => {
+    try {
+        await Like.create({
+            user_id: userId,
+            board_id: boardId,
+        });
+        const likeCount = await Like.countDocuments({ board_id: boardId });
+
+        await Board.findByIdAndUpdate(boardId, { like_count: likeCount });
+        return {
+            status: 201,
+            is_liked: true,
+            message: '좋아요 생성',
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+// 좋아요 취소
+exports.deleteLiked = async (boardId, userId) => {
+    try {
+        const result = await Like.findOneAndDelete({ board_id: boardId, user_id: userId });
+
+        if (!result) {
+            return {
+                status: 400,
+                message: '좋아요 취소 실패'
+            }
+        }
+        const likeCount = await Like.countDocuments({ board_id: boardId });
+
+        await Board.findByIdAndUpdate(boardId, { like_count: likeCount });
+        return {
+            status: 200,
+            is_liked: false,
+            message: '좋아요 취소'
+        }
+    } catch (error) {
+        throw error;
+    }
+}
